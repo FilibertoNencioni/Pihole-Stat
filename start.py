fe7ea581c8576623f5a3e60fd3baa9cf34f70f86
Filiberto <justnencio@gmail.com>
2020-12-11 20:54:13 +0100

    Added RAM usage and fixed % char missing on CPU percentage

--- start.py
+++ start.py
@@ -2,19 +2,22 @@ import time
 import subprocess
 from sys import stdout
 import curses
+import psutil #need to "pip install psutil" or "pip3 install psutil"
 
 x = 0
 stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 
-def stampa(res, MLine):
-        stdscr.addstr(0, 0, "CPU USAGE: {0}".format(res))
+def stampa(res, MLine, RLine):
+        stdscr.addstr(0, 0, "CPU USAGE: {0}%".format(res))
         stdscr.addstr(1, 0, "USED STORAGE: {0}".format(MLine))
+        stdscr.addstr(2, 0, "RAM USAGE: {0}%".format(RLine))
         stdscr.refresh()
+
 while x != 5:
         #CPU USAGE CALCULATION BY MPSTAT
-        #first of all you need to install mpstat on your pi by the command: sudo apt-get install mpstat
+        #You need to install mpstat on your pi by the command: sudo apt-get install mpstat
         cpuusage = str(subprocess.run('mpstat',capture_output=True))
         CLine = int(cpuusage[-21:-19])
         res = 100-CLine
@@ -22,6 +25,9 @@ while x != 5:
         #STOREGE USE CALCUTATION
         memuse = str(subprocess.run('df',capture_output=True))
         MLine = memuse[155:158]
-        stampa(res, MLine)
 
+        #RAM USAGE CALCULATION
+        RLine = str(psutil.virtual_memory().percent)
+
+        stampa(res, MLine, RLine)
         time.sleep(1)
